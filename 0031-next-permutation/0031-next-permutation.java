19559372391ac37a1a0b0d1c33a029e0e055eac8
Amrit Raj Singh <amritrajsinghh2006@gmail.com>
2026-07-10 11:17:56 +0530

    Time: 0 ms (100%), Space: 44.6 MB (69.1%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    public void nextPermutation(int[] nums) {
+
+        int pivot = -1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        if (pivot != -1) {
+            for (int i = nums.length - 1; i > pivot; i--) {
+                if (nums[i] > nums[pivot]) {
+                    swap(nums, i, pivot);
+                    break;
+                }
+            }
+        }
+
+        reverse(nums, pivot + 1, nums.length - 1);
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
