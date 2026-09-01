a8b44be672bbe75aba9996349a4a7b90b569cdb3
Amrit Raj Singh <amritrajsinghh2006@gmail.com>
2026-09-01 23:41:01 +0530

    Time: 0 ms (100%), Space: 43.3 MB (61.85%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        int carry = 1;
+        for(int i = n-1; i>=0; i--){
+            int sum = digits[i]+carry;
+            digits[i] = sum%10;
+            carry = sum/10;
+        }
+        if(carry == 1){
+            int ans [] = new int [n+1];
+            ans[0] = 1;
+            return ans;
+        }
+        return digits;
+    }
+}
